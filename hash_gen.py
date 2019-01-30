8f3bb7a4363b0524c9df314503aa7ea9f5e2bba8
mitchya1 <mitchell.anderson@colorado.edu>
2019-01-30 13:35:19 -0700

    search 256 working

--- hash_gen.py
+++ hash_gen.py
@@ -4,6 +4,7 @@ from api import ReverseIT_API
 parser = argparse.ArgumentParser(description="Generate SHA256 hashes and optionally check them against the Reverse.it API")
 parser.add_argument('file', help="Path to file to hash")
 parser.add_argument('-a', '--api', default=False, action="store_true", help="Check SHA256 against API")
+parser.add_argument('-m', '--mitre', default=False, action="store_true", help="Show MITRE info")
 args = parser.parse_args()
 
 
@@ -33,9 +34,105 @@ def main_func():
             else:
                 json_respose = json.loads(api_response)
                 for item in json_respose:
-                    JOB_ID = item['job_id']
+                    job_id = item['job_id']
+                    rit_env = item['environment_description']
+                    malware_type = item['type']
+                    malware_name = item['submit_name']
+                    threat_score = item['threat_score']
+                    threat_level = item['threat_level']
+                    verdict = item['verdict']
+                    domains = ""
+                    for domain in item['domains']:
+                        domains += domain
+                        domains += "\n"
+                    processes_spawned = item['total_processes']
 
-                    print(item['job_id'])
+                    if args.mitre:
+                        msg = """
+Analysis for {} || SHA256: {}
+
+Overall Stats:
+==================================
+
+Verdict: {}
+
+Threat Score: {}
+
+Threat Level: {}
+
+==================================
+Details:
+
+Malware Name: {}
+
+Malware Type: {}
+
+Reverse IT Environment: {}
+
+Total Number of Processes Spawned: {}
+
+Domains Contacted:
+{}
+
+===================================
+
+MITRE INFO:
+
+                        """.format(str(file_), str(sha256_hash), verdict, str(threat_score), str(threat_level),\
+                            str(malware_name), str(malware_type), str(rit_env), str(processes_spawned), str(domains))
+                        for attack_type in item['mitre_attcks']:
+                            tactic = attack_type['tactic']
+                            technique = attack_type['technique']
+                            attack_link = attack_type['attck_id_wiki']
+                            formatted_msg = """
+
+Tactic: {}
+
+Technique: {}
+
+Wiki Link: {}
+
+                            """.format(str(tactic), str(technique), str(attack_link))
+                            msg += formatted_msg
+                        
+                        print(msg)
+
+                        
+                    else:
+                        msg = """
+Analysis for {} || SHA256: {}
+
+Overall Stats:
+==================================
+
+Verdict: {}
+
+Threat Score: {}
+
+Threat Level: {}
+
+==================================
+Details:
+
+Malware Name: {}
+
+Malware Type: {}
+
+Reverse IT Environment: {}
+
+Total Number of Processes Spawned: {}
+
+Domains Contacted:
+
+{}
+
+                        """.format(str(file_), str(sha256_hash), verdict, str(threat_score), str(threat_level),\
+                            str(malware_name), str(malware_type), str(rit_env), str(processes_spawned), str(domains))
+                        
+                        print(msg)
+
+
+
         else:
             print("[X] Not checking against API")
 
